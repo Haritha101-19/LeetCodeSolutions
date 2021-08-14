3e5a05e07c83c8e7b5e04584eae7e245511f993f
Venkata Haritha <85028580+Haritha101-19@users.noreply.github.com>
2021-08-14 20:11:55 +0530

    Create Number of Good Pairs.java

---
+++ Number of Good Pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        if(nums.length==0){
+            return -1;
+        }
+        Arrays.sort(nums);
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]!=nums[j]){
+                    break;
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
